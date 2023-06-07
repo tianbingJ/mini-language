f93bda6a06f24f3414e07a21bf7e093965bf5a73
tianbing <tianbing@kuaishou.com>
2023-06-07 15:33:35 +0800

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author tianbing <tianbing@kuaishou.com>
+ * Created on 2023-06-07
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
